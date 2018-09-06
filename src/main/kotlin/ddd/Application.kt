03be6a9e2ce8693e79d478eb811983e62dfaf4e7
Takahiro Suzuki <bur8787@gmail.com>
2018-09-07 02:07:00 +0900

    add HelloController

--- src/main/kotlin/ddd/Application.kt
+++ src/main/kotlin/ddd/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DddApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<DddApplication>(*args)
+    runApplication<Application>(*args)
 }
